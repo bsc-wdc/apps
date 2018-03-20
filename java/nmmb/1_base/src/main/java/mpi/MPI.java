f2d6a7ca4fa345e910295d50bb13051c14737c8f
Pol.Alvarez@BSC <pol.avms@gmail.com>
2018-03-20 18:03:54 +0100

    nmmb java apps added

---
+++ java/nmmb/1_base/src/main/java/mpi/MPI.java
@@ -0,0 +1,13 @@
+package mpi;
+
+/**
+ * Fake MPI tasks implementation
+ *
+ */
+public class MPI {
+
+    public static Integer nems(String stdOutFile, String stdErrFile) {
+        return null;
+    }
+
+}
