1c1ccfd4dc4624734aa25befc83fba13a20e4444
fconejer <francisco.conejero@bsc.es>
2018-05-31 14:26:47 +0200

    Unified dataset filename.
    Former-commit-id: 96da59aace67ab5a7884f77f6c43a2108aae34a5

--- python/pycompss_lib/algorithms/sort/base/src/generator.py
+++ python/pycompss_lib/algorithms/sort/base/src/generator.py
@@ -7,20 +7,21 @@ import pickle
 def main():
     numbers = 102400
     maxN = 200000
+    dataset_file="dataset.txt"
 
     nums = random.random_integers(maxN, size=(numbers,))
     # Plain numbers output
-    with open("dataset.txt", 'w') as dataset:
+    with open(dataset_file, 'w') as dataset:
         for n in nums:
             dataset.write(str(n) + ' ')
     '''
     # Pickled output
     print("Nums: %s" % str(nums))
-    ff = open('n.txt', 'w')
+    ff = open(dataset_file, 'w')
     pickle.dump(nums, ff)
     ff.close()
 
-    f = open('n.txt', 'r')
+    f = open(dataset_file, 'r')
     aux = pickle.load(f)
     print(aux)
     '''
