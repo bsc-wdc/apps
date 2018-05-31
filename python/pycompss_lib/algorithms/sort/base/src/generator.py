b1fcd54478761c8787c4203c25afab876e8e91c8
fconejer <francisco.conejero@bsc.es>
2018-05-31 10:58:37 +0200

    Updated generator structure.
    Former-commit-id: f36cb9905383573b54ddb34aa485a621dace1893

--- python/pycompss_lib/algorithms/sort/base/src/generator.py
+++ python/pycompss_lib/algorithms/sort/base/src/generator.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
 from numpy import *
 import pickle
 
-numbers = 102400
-#numbers = 10
-maxN = 200000
 
-nums = random.random_integers(maxN,size=(numbers,))
-print nums
-print type(nums)
-ff = open('n.txt','w')
-pickle.dump(nums,ff)
-ff.close()
+def main():
+    numbers = 102400
+    maxN = 200000
 
-f = open('n.txt','r')
-aux = pickle.load(f)
-print(aux)
+    nums = random.random_integers(maxN, size=(numbers,))
+    print("Nums: %s" % str(nums))
+    ff = open('n.txt', 'w')
+    pickle.dump(nums, ff)
+    ff.close()
+
+    f = open('n.txt', 'r')
+    aux = pickle.load(f)
+    print(aux)
+
+
+if __name__ == '__main__':
+    main()
