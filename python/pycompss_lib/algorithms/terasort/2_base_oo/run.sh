35ab82a581b20221fdba9f6768fdace60bf93bc7
fconejer <francisco.conejero@bsc.es>
2018-06-01 16:39:40 +0200

    [Python][Terasort] Added run scripts.
    Former-commit-id: e532cc9f607524c60978e3087ed94daab749b347

---
+++ python/pycompss_lib/algorithms/terasort/2_base_oo/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+  # Define script variables
+  scriptDir=$(pwd)/$(dirname $0)
+  execFile=${scriptDir}/src/terasort.py
+  appClasspath=${scriptDir}/src/
+  appPythonpath=${scriptDir}/src/
+
+  # Retrieve arguments
+  tracing=$1
+
+  # Leave application args on $@
+  shift 1
+
+  # Enqueue the application
+  runcompss \
+    --tracing=$tracing \
+    --classpath=$appClasspath \
+    --pythonpath=$appPythonpath \
+    --lang=python \
+    $execFile $@
+
+
+######################################################
+# APPLICATION EXECUTION EXAMPLE
+# Call:
+#       ./run.sh tracing numFragments numEntries
+#
+# Example:
+#       ./launch.sh false 10 100
+#
