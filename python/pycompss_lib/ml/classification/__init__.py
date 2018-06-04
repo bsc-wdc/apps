fc7be4c2d6520c94b19eaca22654d4a1f145f7cd
Salvi Solà Martinell <salvi.solamartinell@bsc.es>
2018-06-04 11:06:14 +0200

    added random_forest to __init__.py
    Former-commit-id: c50d30a4c91001dd5932d4451f1205bacf069e28

--- python/pycompss_lib/ml/classification/__init__.py
+++ python/pycompss_lib/ml/classification/__init__.py
@@ -1,4 +1,4 @@
 from csvm.base.src.cascadesvm import CascadeSVM
-from random_forest.base.cascadesvm import CascadeSVM
+from random_forest.base.forest.compss_RF_sklearn_trees import RandomForestClassifier
 
-__all__ = ['CascadeSVM']
+__all__ = ['CascadeSVM', 'RandomForestClassifier']
