af2ab9f9345f2e52c784817d0ba3a8e51148b1ff
fconejer <francisco.conejero@bsc.es>
2018-06-04 14:59:52 +0200

    [Python][Mandelbrot] Pep8, python 2 and 3, cleanup..
    Former-commit-id: 565fe4a4d8622192bc346ccf4eb6d86c2ae09e55

--- python/examples/mandelbrot/1_base/src/mandel.py
+++ python/examples/mandelbrot/1_base/src/mandel.py
@@ -17,8 +17,9 @@
 
 # -*- coding: utf-8 -*-
 
+import sys
+import time
 from pycompss.api.task import task
-from pycompss.api.parameter import *
 from numpy import NaN, arange, abs, array
 
 
@@ -32,30 +33,34 @@ def m(a, i=100):
 
 
 @task(returns=list)
-def groupTasks(y, X, n):
-    Z = [0 for _ in xrange(len(X))]
+def group_tasks(y, X, n):
+    Z = [0 for _ in range(len(X))]
     for ix, x in enumerate(X):
         Z[ix] = m(x + 1j * y, n)
     return Z
 
-if __name__ == "__main__":
-    import sys
-    import time
+
+def mandelbrot(X, Y, Z, points):
     from pycompss.api.api import compss_wait_on
-    X = arange(-2, .5, .01)
-    Y = arange(-1.0,  1.0, .01)
-    Z = [[] for _ in xrange(len(Y))]
-    n = int(sys.argv[1])
-    
+
     st = time.time()
     for iy, y in enumerate(Y):
-        Z[iy] = groupTasks(y,X, n)
+        Z[iy] = group_tasks(y, X, points)
     Z = compss_wait_on(Z)
-    print "Ellapsed time (s): {}".format(time.time()-st)
-    
+    print("Elapsed time (s): {}".format(time.time() - st))
+
     # Plot Result
-    #import matplotlib.pyplot as plt
-    #Z = array(Z)
-    #plt.imshow(Z, cmap='spectral')
-    #plt.show()
-    #plt.imsave('Mandelbrot',Z, cmap='spectral')
+    import matplotlib.pyplot as plt
+    Z = array(Z)
+    plt.imshow(Z, cmap='spectral')
+    # plt.show()
+    plt.imsave('Mandelbrot.png', Z, cmap='spectral')
+
+
+if __name__ == "__main__":
+    X = arange(-2, .5, .01)
+    Y = arange(-1.0,  1.0, .01)
+    Z = [[] for _ in range(len(Y))]
+    n = int(sys.argv[1])
+
+    mandelbrot(X, Y, Z, n)
