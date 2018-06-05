bd0d599bd07e60b41d6977417885ebc0e68eec44
fconejer <francisco.conejero@bsc.es>
2018-06-05 09:58:48 +0200

    [Python][Wordcount] Version 4 - Pep8. Python 2 and 3. Cleanup.
    Former-commit-id: 53da6f0033fbb0faaff0817e434a3f7792255926

--- python/examples/wordcount/4_in/src/wordcount.py
+++ python/examples/wordcount/4_in/src/wordcount.py
@@ -17,29 +17,33 @@
 
 # -*- coding: utf-8 -*-
 
-'''Wordcount self read'''
+"""Wordcount self read"""
+
+import sys
 import pickle
 import time
 from pycompss.api.task import task
-from pycompss.api.parameter import *
+from functools import reduce
 
-@task(returns=dict,priority=True)
-def wordCount_selfRead(pathFile,start,sizeBlock):
-    fp = open(pathFile)
+
+@task(returns=dict, priority=True)
+def wordcount_self_read(path_file, start, size_block):
+    fp = open(path_file)
     fp.seek(start)
-    aux = fp.read(sizeBlock)
+    aux = fp.read(size_block)
     fp.close()
     data = aux.strip().split(" ")
-    partialResult = {}
+    partial_result = {}
     for entry in data:
-        if entry not in partialResult:
-            partialResult[entry] = 1
+        if entry not in partial_result:
+            partial_result[entry] = 1
         else:
-            partialResult[entry] = partialResult[entry] + 1
-    return partialResult
+            partial_result[entry] = partial_result[entry] + 1
+    return partial_result
+
 
 @task(returns=dict)
-def reduce(dic1,dic2):
+def reduce(dic1, dic2):
     for k in dic2:
         if k in dic1:
             dic1[k] = dic1[k] + dic2[k]
@@ -47,49 +51,51 @@ def reduce(dic1,dic2):
             dic1[k] = dic2[k]
     return dic1
 
-def mergeReduce(partialResult):
-    n = len(partialResult)
+
+def merge_reduce(partial_result):
+    n = len(partial_result)
     act = [j for j in range(n)]
     while n > 1:
         aux = []
-        if n%2:
-            partialResult[act[len(act)-2]]=reduce(partialResult[act[len(act)-2]], partialResult[act[len(act)-1]])
+        if n % 2:
+            partial_result[act[len(act)-2]] = reduce(partial_result[act[len(act)-2]], partial_result[act[len(act)-1]])
             act.pop(len(act)-1)
             n = n-1
-        for i in range(0,n,2):
-            partialResult[act[i]]=reduce(partialResult[act[i]],partialResult[act[i+1]])
+        for i in range(0, n, 2):
+            partial_result[act[i]] = reduce(partial_result[act[i]], partial_result[act[i+1]])
             aux.append(act[i])
         act = aux
         n = len(act)
+    return partial_result[0]
 
-    partialResult[0] = compss_wait_on(partialResult[0])
-    return partialResult[0]
 
-if __name__ == "__main__":
-    import sys
-    import os
+def main():
     from pycompss.api.api import compss_wait_on
-    pathFile = sys.argv[1]
-    resultFile = sys.argv[2]
-    sizeBlock = int(sys.argv[3])
 
-    print "Start"
+    path_file = sys.argv[1]
+    result_file = sys.argv[2]
+    size_block = int(sys.argv[3])
+
     start = time.time()
-    data = open(pathFile)
-    data.seek(0,2)
+    data = open(path_file)
+    data.seek(0, 2)
     file_size = data.tell()
     ind = 0
-    partialResult = []
+    partial_result = []
     while ind < file_size:
-        partialResult.append(wordCount_selfRead(pathFile, ind, sizeBlock))
-        ind += sizeBlock
-    result = mergeReduce( partialResult)
-    end = time.time()-start
-    print "Ellapsed Time"
-    print end
+        partial_result.append(wordcount_self_read(path_file, ind, size_block))
+        ind += size_block
+    result = merge_reduce(partial_result)
+    result = compss_wait_on(result)
 
-    #aux = list(result.items())
-    #ff = open(resultFile,'w')
-    #pickle.dump(aux,ff)
-    #ff.close()
+    print("Elapsed Time")
+    print(time.time()-start)
 
+    aux = list(result.items())
+    ff = open(result_file, 'w')
+    pickle.dump(aux, ff)
+    ff.close()
+
+
+if __name__ == "__main__":
+    main()
