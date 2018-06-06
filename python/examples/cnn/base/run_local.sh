92eedc95b350849303ee8c96d258c56cbfac7814
Pol.Alvarez@BSC <pol.avms@gmail.com>
2018-06-06 14:54:54 +0200

    finished CNN revision
    Former-commit-id: 7d7b5fc7770812bb5b938547799d33ea70ac9ebe

---
+++ python/examples/cnn/base/run_local.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+  # Define script variables
+  scriptDir=$(dirname $0)
+  execFile=src/compss_mnist.py
+  appClasspath=${scriptDir}/src/
+  appPythonpath=${scriptDir}/src/
+
+  # Retrieve arguments
+  tracing=$1
+
+  # Leave application args on $@
+  shift 1
+
+  # Enqueue the application
+  runcompss \
+    --tracing=$tracing \
+    --classpath=$appClasspath \
+    --pythonpath=$appPythonpath \
+    --lang=python \
+    $execFile $@
+
+
+######################################################
+# APPLICATION EXECUTION EXAMPLE
+# Call:
+#       ./run_local.sh tracing numModels 
+#
+# Example:
+#       ./run_local.sh false 2
+#
+
