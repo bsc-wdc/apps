9468aff1f340abf06b386a81b63fa63af7ac3d1c
Salvi Solà Martinell <salvi.solamartinell@bsc.es>
2018-07-12 15:57:35 +0200

    RandomForest locally enabling VisualVM

--- python/pycompss_lib/ml/classification/random_forest/full/sample/launch_local_rf.sh
+++ python/pycompss_lib/ml/classification/random_forest/full/sample/launch_local_rf.sh
@@ -3,6 +3,7 @@ BASE_PATH="/home/bscuser/git"
 #     --qos=debug \
 #     --tracing=true \
 runcompss \
+    --jvm_master_opts="-Dcom.sun.management.jmxremote.port=3333,-Dcom.sun.management.jmxremote.ssl=false,-Dcom.sun.management.jmxremote.authenticate=false" \
     --pythonpath="${BASE_PATH}/apps/python/pycompss_lib/ml/classification/random_forest/full/src" \
     main_random_forest.py\
         --path_in=/home/bscuser/datasets/dt_test_4/ \
