f4428a9b6ec3ccf5755bab00aa708a991845af93
Javier Alvarez <javier.alvarez@bsc.es>
2018-07-25 11:12:24 +0200

    [C-SVM] Removed has_duplicates parameter (i.e., always check for duplicates)

--- python/pycompss_lib/ml/classification/csvm/base/src/cascadesvm.py
+++ python/pycompss_lib/ml/classification/csvm/base/src/cascadesvm.py
@@ -298,7 +298,7 @@ class CascadeSVM(object):
                     # first level
                     for chunk in chunks:
                         data = filter(None, [chunk, feedback[idx]])
-                        q.append(train(False, True, *data, **self._clf_params[idx]))
+                        q.append(train(False, *data, **self._clf_params[idx]))
 
                     # reduction
                     while len(q) > 1:
@@ -308,9 +308,9 @@ class CascadeSVM(object):
                             data.append(q.popleft())
 
                         if q or not check_convergence:
-                            q.append(train(False, False, *data, **self._clf_params[idx]))
+                            q.append(train(False, *data, **self._clf_params[idx]))
                         elif not q:
-                            sv, sl, self._clf[idx] = compss_wait_on(train(True, False, *data, **self._clf_params[idx]))
+                            sv, sl, self._clf[idx] = compss_wait_on(train(True, *data, **self._clf_params[idx]))
                             q.append((sv, sl))
 
                         # delete partial results
@@ -429,9 +429,9 @@ class CascadeSVM(object):
 
 
 @task(returns=tuple)
-def train(return_classifier, has_duplicates, *args, **kwargs):
+def train(return_classifier, *args, **kwargs):
     if len(args) > 1:
-        X, y = merge(has_duplicates, *args)
+        X, y = merge(*args)
     else:
         X, y = args[0]
 
@@ -467,14 +467,14 @@ def read_chunk(filename, start=None, stop=None, data_format=None, n_features=Non
     return X, y
 
 
-def merge(has_duplicates, *args):
+def merge(*args):
     if issparse(args[0][0]):
-        return merge_sparse(has_duplicates, *args)
+        return merge_sparse(*args)
     else:
-        return merge_dense(has_duplicates, *args)
+        return merge_dense(*args)
 
 
-def merge_dense(has_duplicates, *args):
+def merge_dense(*args):
     sv1, sl1 = args[0]
     sv1 = np.concatenate((sv1, sl1[:, np.newaxis]), axis=1)
 
@@ -483,52 +483,46 @@ def merge_dense(has_duplicates, *args):
         sv2 = np.concatenate((sv2, sl2[:, np.newaxis]), axis=1)
         sv1 = np.concatenate((sv1, sv2))
 
-    if has_duplicates:
-        sv1 = np.unique(sv1, axis=0)
+
+    sv1 = np.unique(sv1, axis=0)
 
     return sv1[:, :-1], sv1[:, -1]
 
 
-def merge_sparse(has_duplicates, *args):
-    if not has_duplicates:
-        sv = vstack([t[0] for t in args])
-        sl = np.concatenate([t[1] for t in args])
+def merge_sparse(*args):
+    sv1, sl1 = args[0]
+    sv1 = sv1.asformat("lil")
 
-        return sv, sl
-    else:
-        sv1, sl1 = args[0]
-        sv1 = sv1.asformat("lil")
+    rows = sv1.rows.tolist()
+    data = sv1.data.tolist()
 
-        rows = sv1.rows.tolist()
-        data = sv1.data.tolist()
+    for t2 in args[1:]:
+        sv2, sl2 = t2
+        sv2 = sv2.asformat("lil")
+        nrows = len(sv2.rows)
 
-        for t2 in args[1:]:
-            sv2, sl2 = t2
-            sv2 = sv2.asformat("lil")
-            nrows = len(sv2.rows)
+        for i in range(nrows):
+            duplicate = False
 
-            for i in range(nrows):
-                duplicate = False
+            for j in range(len(rows)):
+                # check if column indices are the same
+                if set(sv2.rows[i]) == set(rows[j]):
 
-                for j in range(len(rows)):
-                    # check if column indices are the same
-                    if set(sv2.rows[i]) == set(rows[j]):
+                    # check if data is the same
+                    data1 = data[j]
+                    data2 = sv2.data[i]
 
-                        # check if data is the same
-                        data1 = data[j]
-                        data2 = sv2.data[i]
+                    if np.allclose(data1, data2):
+                        duplicate = True
+                        break
 
-                        if np.allclose(data1, data2):
-                            duplicate = True
-                            break
+            if not duplicate:
+                rows.append(sv2.rows[i])
+                data.append(sv2.data[i])
+                sl1 = np.concatenate((sl1, sl2[i, np.newaxis]))
 
-                if not duplicate:
-                    rows.append(sv2.rows[i])
-                    data.append(sv2.data[i])
-                    sl1 = np.concatenate((sl1, sl2[i, np.newaxis]))
+    svs = lil_matrix((len(rows), sv1.shape[1]))
+    svs.rows = np.array(rows, dtype=list)
+    svs.data = np.array(data, dtype=list)
 
-        svs = lil_matrix((len(rows), sv1.shape[1]))
-        svs.rows = np.array(rows, dtype=list)
-        svs.data = np.array(data, dtype=list)
-
-        return svs.asformat("csr"), sl1
+    return svs.asformat("csr"), sl1
