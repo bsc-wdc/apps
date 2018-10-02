5c94953201a13eacdad81801dc0d8e14a031b396
CristianRCV <cristianrcv@gmail.com>
2018-10-02 09:26:59 +0200

    [C][K-means] Pythonic generate_files

--- c/K-means/generator/generate_files.py
+++ c/K-means/generator/generate_files.py
@@ -10,40 +10,41 @@ import pickle
 # python generate_files.py 2000 5 10 4
 
 
-def generate_data(numV, dim, K):
-    n = int(float(numV) / K)
+def generate_data(num_v, dim, k):
+    n = int(float(num_v) / k)
     data = []
     random.seed(5)
-    for k in range(K):
-        c = [random.uniform(-1, 1) for i in range(dim)]
+    for k in range(k):
+        c = [random.uniform(-1, 1) for _ in range(dim)]
         s = random.uniform(0.05, 0.5)
-        for i in range(n):
+        for _ in range(n):
             d = np.array([np.random.normal(c[j], s) for j in range(dim)])
             data.append(d)
 
-    Data = np.array(data)[:numV]
-    return Data
+    array_data = np.array(data)[:num_v]
+    return array_data
+
+
+def write_to_file(n, k, dim, ind):
+    text_file = str("N" + str(n) + "_K" + str(k) + "_d" + str(dim) + "_" + str(ind) + ".txt")
+    with open(text_file, 'w') as ff:
+        for i in range(len(x)):
+            lines = str(i + 1) + ' '
+            for j in range(len(x[i])):
+                lines += str(x[i][j]) + ' '
+            lines += '\n'
+            ff.write(lines)
+
 
 if __name__ == "__main__":
     N = int(sys.argv[1])
     K = int(sys.argv[2])
-    dim = int(sys.argv[3])
+    dimension = int(sys.argv[3])
     numFrag = int(sys.argv[4])
 
-    nums_per_frag = int(N/numFrag)
+    nums_per_frag = int(N / numFrag)
 
-    for ind in range(numFrag):
-        x = generate_data(nums_per_frag, dim, K)
+    for index in range(numFrag):
+        x = generate_data(nums_per_frag, dimension, K)
         print x
-        textFile = str("N"+str(N)+"_K"+str(K)+"_d"+str(dim)+"_"+str(ind)+".txt")
-        ff = open(textFile, 'w')
-        for i in range(len(x)):
-            lines = ""
-	    lines += str(i+1)+' '
-            for j in range(len(x[i])):
-                lines += str(x[i][j])+' '
-            lines += '\n'
-            ff.write(lines)
-        ff.close()
-
-
+        write_to_file(N, K, dimension, index)
