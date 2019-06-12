4776088cb4fa2ff486d676b68a8d287c9fee1281
fconejer <javier.conejero@bsc.es>
2019-06-12 12:10:00 +0200

    Updated wordcount app.
    Former-commit-id: 54a517a79b440d034400abd492cf472e17daddb5

--- python/examples/wordcount/clean.sh
+++ python/examples/wordcount/clean.sh
@@ -7,12 +7,13 @@
   # Clean temp files
   find . -type f -name '*.sh~' -delete
   find . -type f -name '*.py~' -delete
- 
+
   # Clean python compiled files
   find . -type f -name '*.pyc' -delete
- 
+
   # Clean trace files
   find . -type f -name '*tar.gz' -delete
 
   # Remove temporal dataset
-  rm -rf dataset_*
+  find . -type f -name 'file*.txt' -delete
+  find . -type d -name 'dataset_*' -delete
