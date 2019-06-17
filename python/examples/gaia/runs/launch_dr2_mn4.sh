e3a8db79837827837c50e8ebff1c554a5b1827d1
Salvi Solà Martinell <salvi.solamartinell@bsc.es>
2019-06-17 17:14:32 +0200

    removed gaia (moved to a new repo)
    Former-commit-id: 91702a54903d4941a0d7a1127e04d49ce28a8211

--- python/examples/gaia/runs/launch_dr2_mn4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# File paths have to be adapted: writing permissions are required for working_worker_dir, base_log_dir and -o (output) files. The .pkl file is a pickled version of the dataset.
-# Usage: ./launch_dr2.py <L> <minPts>
-
-enqueue_compss --constraints=highmem --lang=python --scheduler=es.bsc.compss.scheduler.fifoDataScheduler.FIFODataScheduler --worker_in_master_cpus=0 --max_tasks_per_node=24 --worker_working_dir=/gpfs/scratch/bsc19/bsc19029/ --exec_time=2880 --num_nodes=3 --base_log_dir=/gpfs/scratch/bsc19/bsc19029/ /home/bsc19/bsc19029/gaia/clustering_dr2.py -L $1 -m $2 -o /home/bsc19/bsc19029/gaia/out_dr2 /gpfs/projects/bsc19/COMPSs_DATASETS/dislib/gaia/df_dr2.pkl 
