9e1a1df6124bf1439be97e2a5e3b314a9e78d286
fconejer <javier.conejero@bsc.es>
2020-04-06 17:57:45 +0200

    Fixed Hecuba launcher.

--- python/examples_with_persistent_storage/kmeans_OO/launch_with_Hecuba.sh
+++ python/examples_with_persistent_storage/kmeans_OO/launch_with_Hecuba.sh
@@ -14,7 +14,9 @@
   fi
 
   export COMPSS_PYTHON_VERSION=3-ML
-  module load COMPSs/2.6.3
+  # module load COMPSs/2.6.3
+  module use /apps/modules/modulefiles/tools/COMPSs/.custom
+  module load TrunkJCB
   module load hecuba/0.1.3_ML
 
   # Retrieve script arguments
