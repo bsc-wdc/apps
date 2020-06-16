eccb17cd4ac0fd5d794f313fb17037d5b56aaa5c
fconejer <javier.conejero@bsc.es>
2020-06-16 13:20:56 +0200

    Added PYTHONPATH to kmeans run scripts.

--- python/examples_with_persistent_storage/kmeans_OO/run.sh
+++ python/examples_with_persistent_storage/kmeans_OO/run.sh
@@ -1,4 +1,5 @@
 runcompss \
   -dg \
   --python_interpreter=python3 \
+  --pythonpath=$(pwd)/src \
   src/kmeans.py -n 1024 -f 8 -d 2 -c 4
--- python/examples_with_persistent_storage/kmeans_OO/run_with_redis.sh
+++ python/examples_with_persistent_storage/kmeans_OO/run_with_redis.sh
@@ -4,6 +4,7 @@
 runcompss \
   -d \
   --python_interpreter=python3 \
+  --pythonpath=$(pwd)/src \
   --storage_impl=redis \
   --storage_conf=$(pwd)/redis_confs/storage_conf.cfg \
   src/kmeans.py -n 1024 -f 8 -d 2 -c 4 --use_storage
